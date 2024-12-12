35d45ae385043e0d7961216d400784950543ad1e
Ishan Taunk <ishan.taunk444@gmail.com>
2024-12-12 21:37:16 +0530

    Added API calls using mongoose

--- src/app.js
+++ src/app.js
@@ -3,16 +3,40 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async(req,res) => {
+app.use(express.json());
 
-    const user = new User({
-        firstName: "virat",
-        lastName: "kohli",
-        email: "ishan.taunk444@gmail.com",
-        password: "password",
-        age: 30,
-        gender: "M"
-    })
+app.patch("/user", async(req,res)=>{
+    const userEmail = req.body.emailId;
+    console.log(req.body);
+    try{
+        const user = await User.findOneAndUpdate({emailId:userEmail},req.body,{returnDocument:'after'});
+        if(user){
+            res.send(user);
+        }else{
+            res.status(404).send("User not found");
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({});
+        if(users.length===0){
+            res.status(404).send("User not found!");
+        }else{
+            res.send(users);
+        }
+    } catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.post("/signup", async(req,res) => {
+    console.log(req.body);
+    const user = new User(req.body);
 
     try{
         await user.save();
